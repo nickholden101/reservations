60ff91864d35a4ef4204c325f2d4d75e28298c99
nickholden101 <nickholden101@users.noreply.github.com>
2017-03-29 15:58:09 +0100

    Add files via upload

--- app/assets/javascripts/datepickers.js
+++ app/assets/javascripts/datepickers.js
@@ -1,11 +1,11 @@
 function load_datepicker() {
   $('.date_start').datetimepicker({
     // altField: '#date_start_alt',
-    // altFormat: 'yy-mm-dd hh:mm',
+    // altFormat: 'dd-mm-yy hh:mm',
     // timeInput: true,
     // timeFormat: "hh:mm",
     dateFormat: 'yy-mm-dd',
-    timeFormat: "hh:mm:ss",
+    timeFormat: "hh:mm",
     minDate: 0,
     onClose: function(dateText, inst) {
       var start_date = $('.date_start').datetimepicker("getDate");
@@ -20,37 +20,39 @@ function load_datepicker() {
 
   $('.date_end').datetimepicker({
     // altField: '#date_end_alt',
-    // altFormat: 'yy-mm-dd hh:mm',
+    // altFormat: 'dd-mm-yy hh:mm',
     dateFormat: 'yy-mm-dd',
-    timeFormat: "hh:mm:ss",
+    timeFormat: "hh:mm",
     minDate: 0
   });
 
-  $('.date_start_no_min').datepicker({
-    altField: '#date_start_alt',
-    altFormat: 'yy-mm-dd',
+  $('.date_start_no_min').datetimepicker({
+    dateFormat: 'dd-mm-yy',
+    timeFormat: "hh:mm",
     onClose: function(dateText, inst) {
-      var start_date = $('.date_start_no_min').datepicker("getDate");
-      var end_date = $('.date_end_no_min').datepicker("getDate");
+      var start_date = $('.date_start_no_min').datetimepicker("getDate");
+      var end_date = $('.date_end_no_min').datetimepicker("getDate");
+      $(this).next().val($(this).val());
       if (start_date > end_date){
         var new_date = new Date(start_date.getTime()+86400000);
-        $('.date_end_no_min').datepicker("setDate", new_date);
+        $('.date_end_no_min').datetimepicker("setDate", new_date);
       }
-      $('.date_end_no_min').datepicker( "option" , "minDate" , start_date);
+      $('.date_end_no_min').datetimepicker( "option" , "minDate" , start_date);
     }
   });
 
-  $('.date_end_no_min').datepicker({
-    altField: '#date_end_alt',
-    altFormat: 'yy-mm-dd',
+  $('.date_end_no_min').datetimepicker({
+    dateFormat: 'dd-mm-yy',
+    timeFormat: "hh:mm",
     onClose: function(dateText, inst) {
-      var start_date = $('.date_start_no_min').datepicker("getDate");
-      var end_date = $('.date_end_no_min').datepicker("getDate");
+      var start_date = $('.date_start_no_min').datetimepicker("getDate");
+      var end_date = $('.date_end_no_min').datetimepicker("getDate");
+      $(this).next().val($(this).val());
       if (start_date > end_date) {
         var new_date = new Date(start_date.getTime()+86400000);
-        $('.date_end_no_min').datepicker("setDate", new_date);
+        $('.date_end_no_min').datetimepicker("setDate", new_date);
       }
-      $('.date_end_no_min').datepicker( "option" , "minDate" , start_date);
+      $('.date_end_no_min').datetimepicker( "option" , "minDate" , start_date);
     }
   });
 };
